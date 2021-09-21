53946fbdce9a9708cda92c70ae1abc88db97646c
deesnest <87329773+deesnest@users.noreply.github.com>
2021-09-21 22:26:20 +0530

    Update provider.tf

--- singleinstances/provider.tf
+++ singleinstances/provider.tf
@@ -1,5 +1,5 @@
 provider "aws"{
-access_key = "AKIAWXL533ZHOG57OFIJ"
-secret_key = "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"
+access_key = "<access key>"
+secret_key = "<secret key>"
 region = "ap-south-1"
 }
